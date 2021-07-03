eb5f2d10ea0500e04dabc9163d1405eda47e88a6
PhalgunaRao <phalu900@gmail.com>
2021-07-03 22:20:51 +0530

    First Coroutine  and delay

--- app/src/main/java/com/example/androiddemoall/MainActivity.kt
+++ app/src/main/java/com/example/androiddemoall/MainActivity.kt
@@ -2,20 +2,26 @@ package com.example.androiddemoall
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.liveData
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
 import retrofit2.Response
 
 class MainActivity : AppCompatActivity() {
+    val TAG="MainActivity"
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val retService:AlbumService=RetrofitInstance
-            .getRetrofitInstance().create(AlbumService::class.java)
-        val responseLiveData: LiveData<Response<Albums>> = liveData {
+       GlobalScope.launch {
+           delay(10000L)
+           Log.d(TAG,"Coroutin====${Thread.currentThread().name}")
+       }
 
-        }
+        Log.d(TAG,"main thread${Thread.currentThread().name}")
     }
 }
 
