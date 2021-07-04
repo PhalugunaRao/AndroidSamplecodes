7e56383bae6a552db583c65804c98944510c91b0
PhalgunaRao <phalu900@gmail.com>
2021-07-04 12:06:17 +0530

    Coroutine  suspend

--- app/src/main/java/com/example/androiddemoall/MainActivity.kt
+++ app/src/main/java/com/example/androiddemoall/MainActivity.kt
@@ -16,12 +16,29 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+
+        GlobalScope.launch {
+            val networkCallAnswer=doNetworkCall()
+            val networkCallAnswer2=doNetworkCall2()
+            Log.d(TAG,networkCallAnswer)
+            Log.d(TAG,networkCallAnswer2)
+        }
+
+      /* //Coroutine
        GlobalScope.launch {
            delay(10000L)
            Log.d(TAG,"Coroutin====${Thread.currentThread().name}")
        }
+        Log.d(TAG," main thread${Thread.currentThread().name}")*/
+    }
 
-        Log.d(TAG," main thread${Thread.currentThread().name}")
+    suspend fun doNetworkCall():String{
+        delay(3000L)
+        return "This is the answer"
+    }
+    suspend fun doNetworkCall2():String{
+        delay(10000L)
+        return "This is the second answer"
     }
 }
 
